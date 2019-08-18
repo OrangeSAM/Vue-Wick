15be40a425d400f8d971c8d25d969d41e17091bc
Sam <amazing_sam@outlook.com>
2019-08-18 21:25:41 +0800

    初步实现button组件

---
+++ src/button.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <button class="w-button" :class="{[`icon-${iconPosition}`]: true}">
+      <svg v-if="icon" class="icon">
+        <use :xlink:href="`#w-${icon}`" />
+      </svg>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String
+    },
+    iconPosition: {
+      type: String,
+      default: "left"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.w-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+    margin-left: 5px;
+  }
+  > .icon {
+    order: 1;
+  }
+  &.icon-right {
+    > .content {
+      order: 1;
+      margin-left: 0;
+      margin-right: 5px;
+    }
+    > .icon {
+      order: 2;
+    }
+  }
+}
+</style>
