4bc79d2045ab6bae780a0705c8ede01353d77342
Sam <amazing_sam@outlook.com>
2019-10-25 00:21:33 +0800

    思考gutter的问题

--- src/col.vue
+++ src/col.vue
@@ -44,12 +44,14 @@ export default {
             }
         }
         $class-prefix: offset-;
-        @for $n from 1 through 24 {
+        @for $n from 1 through 24 { 
             &.#{$class-prefix}#{$n} {
                 margin-left: ($n / 24) * 100%
             }
         }
         // 问题1，如果是行首的话，这样会少了第一个col 的margin-left: 20px， 参照element才是对的，
         // 问题2，row与col的gutter如何实现，目前是参照MDN官网的row 右padding, col 左margin
+
+        // 还是我现在gutter的设置方式有问题
     }
 </style>
