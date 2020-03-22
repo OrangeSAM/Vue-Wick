252e3605992eddf80d3096656f87687d1833b59b
SamLiu <amazing_sam@outlook.com>
2020-03-22 14:32:52 +0800

    feat: 添加布局组件：layout aside footer header content

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
-import Vue from "vue";
-import Button from "./button";
-import Icon from "./icon";
-import ButtonGroup from "./button-group";
-import Input from "./input";
+import Vue from "vue"
+import Button from "./button"
+import Icon from "./icon"
+import ButtonGroup from "./button-group"
+import Input from "./input"
 import Row from './row'
 import Col from './col'
+import Layout from  './layout'
+import Aside from './aside'
+import Footer from './footer'
+import Content from './content'
+import Header from './header'
 
+
+// 这里注册时的w-button, 是供在template里使用组件时编写的
+// 而在组件内部声明的name, 可以在vue-devtools中看到组件的名称，
+//     可以用来标识组件，但并非唯一。
 Vue.component("w-button", Button);
 Vue.component("w-icon", Icon);
 Vue.component("w-button-group", ButtonGroup);
 Vue.component("w-input", Input);
 Vue.component("w-row", Row)
 Vue.component("w-col", Col)
+Vue.component("w-layout", Layout)
+Vue.component("w-header", Header)
+Vue.component("w-content", Content)
+Vue.component("w-footer", Footer)
+Vue.component("w-aside", Aside)
 new Vue({
   el: "#app",
   data: {
---
+++ src/content.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "w-content"
+  }
+</script>
+
+<style scoped>
+  .content{
+    border: 1px solid lightgray;
+    flex-grow: 1;
+  }
+</style>
