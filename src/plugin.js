826b6828e229b3d985340d83c031adb9ace69c1b
Sam.L <liuqiang@dongni100.com>
2020-05-20 20:57:42 +0800

    feat: 解决currentToast的一个bug

--- src/plugin.js
+++ src/plugin.js
@@ -9,20 +9,19 @@ export default {
       if (currentToast) {
         currentToast.close()
       }
-      currentToast = createToast({Vue, message, propsData: toastOptions})
+      currentToast = createToast({Vue, message, propsData: toastOptions, onClose: () => {currentToast = null}})
     }
   }
 }
-function createToast ({Vue, message, propsData}) {
+function createToast ({Vue, message, propsData, onclose}) {
   let Constructor = Vue.extend(Toast)
   // 这里应优先使用组件而不是 js 原生API
   // 这样，可以在组件内用@click 形式的事件处理，而不是原生的addEventListener
-  let toast = new Constructor({
-    propsData
-  })
+  let toast = new Constructor({propsData})
   // 要放在mount前面
   toast.$slots.default = [message]
   toast.$mount()
+  toast.$on('close', onclose)
   document.body.appendChild(toast.$el)
   return toast
 }
