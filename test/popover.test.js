830e0a5fee3da46cb9e1ecb5095d2821b601aaac
Sam.L <liuqiang@dongni100.com>
2020-07-09 21:05:23 +0800

    feat: popover组件测试

--- test/popover.test.js
+++ test/popover.test.js
@@ -7,6 +7,32 @@ Vue.config.devtools = false;
 
 describe('popover', () => {
   it('存在', () => {
-    expect(Popover).to.be.ok()
+    expect(Popover).to.exist
+  })
+
+  it('可以设置position', (done) => {
+    Vue.component('w-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <w-popover position='bottom' ref='divRef'>
+        <template slot='content'>
+          弹出内容
+        </template>
+        <button>点我</button>
+      </w-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    vm.$el.querySelector('button').click()
+    setTimeout(() => {
+      setTimeout(() => {
+        const {content} = vm.$refs.divRef.$refs
+        console.log(vm.$refs.divRef.$refs)
+        expect(content.classList.contains('position-bottom')).to.be.true
+      }, 500)
+      done()
+    }, 400)
   })
 })
